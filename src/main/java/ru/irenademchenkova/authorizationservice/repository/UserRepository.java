603336c8b60983dad2243be3656c1595aa1a0a88
IrenaDemchenkova <108131996+IrenaDemchenkova@users.noreply.github.com>
2023-04-03 20:36:09 +0300

    fix users

--- src/main/java/ru/irenademchenkova/authorizationservice/repository/UserRepository.java
+++ src/main/java/ru/irenademchenkova/authorizationservice/repository/UserRepository.java
@@ -14,9 +14,9 @@ public class UserRepository {
         User user1 = new User("Ivanova", "xxx", Arrays.asList(Authorities.READ));
         usersList.put(user1.getUser(), user1);
         User user2 = new User("Petrov", "12345", Arrays.asList(Authorities.READ, Authorities.WRITE));
-        usersList.put(user2.getUser(), user1);
+        usersList.put(user2.getUser(), user2);
         User user3 = new User("Sidorov", "54321", Arrays.asList(Authorities.READ, Authorities.WRITE, Authorities.DELETE));
-        usersList.put(user3.getUser(), user1);
+        usersList.put(user3.getUser(), user3);
     }
 
     public List<Authorities> getUserAuthorities(String user, String password) {
